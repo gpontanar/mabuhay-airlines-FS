dc516e5fd64bc3a2c231791d0f53e8ff69dfdcb7
liaalmidaclemente <liaalmidaclemente@gmail.com>
2025-04-07 17:24:22 +0800

    Added login and register modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {useState, useEffect} from 'react';
+
+
 import Home from "./pages/Home";
+import Login from './components/Login';
+import SignUpModal from './components/SignUp';
+import Navbar from './components/AppNavbar';
 
 function App() {
   return (
-    <>
-      <Home />
-    
-    </>
+   <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUpModal />} />
+        <Route path="/login" element={<Login />} />
+     
+      </Routes>
+    </Router>
   );
 }
 
